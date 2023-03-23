bc574ecf16e5fbace5e4b67990f607396e9a5158
runze lu <lrz2364668906@gmail.com>
2023-03-23 16:51:11 +0800

    配置mysql和注册登录模块1

---
+++ backend/src/main/java/com/kob/backend/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kob.backend.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.impl.utils.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {/*alt + insert键*/
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override/*会传入一个username，要返回这个username 对应的信息，这个信息要实现下面这个接口*/
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", username);
+        User user = userMapper.selectOne(queryWrapper);
+        if (user == null) {
+            throw new RuntimeException("用户不存在");
+        }
+        return new UserDetailsImpl(user);
+    }
+}
